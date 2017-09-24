f9d4175cc13f95ba96cd0159fcf47b2243fbed11
S B <S B>
2017-09-24 10:23:59 -0600

    Misc setup

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+	public Camera camera;
+	private GameObject sPrefab;
+	private string mode = "none";
+
+	void Start ()
+	{
+		sPrefab = (GameObject)Resources.Load("Prefabs/Stone", typeof(GameObject));
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	void FixedUpdate ()
+	{
+		if(mode=="throw")
+		{
+
+		}
+	}
+
+	public void beginThrow()
+	{
+		//spawn stone
+	}
+}
