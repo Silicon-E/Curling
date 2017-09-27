e168acd40943c5383207e7c04f9aa0f01ba8b6f1
S B <S B>
2017-09-26 19:59:56 -0600

    Work on stone throwing

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Control : MonoBehaviour {
 
 	public Camera camera;
-	private GameObject sPrefab;
+	public GameObject sPrefab;
+	public Transform stoneSpawn;
+	public float launchSens;
+
 	private string mode = "none";
+	private GameObject stone;
+	Vector3 launch;
 
 	void Start ()
 	{
-		sPrefab = (GameObject)Resources.Load("Prefabs/Stone", typeof(GameObject));
+		//sPrefab = (GameObject)Resources.Load("Prefabs/Stone", typeof(GameObject));
+		//Debug.Log(sPrefab);
+		beginThrow();//TODO: remove when game is structured
 	}
 
 	void Update ()
@@ -20,14 +27,26 @@ public class Control : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
-		if(mode=="throw")
+		if(mode=="launch")
 		{
-
+			launch.x += Input.GetAxis("Mouse X")*launchSens;
+			launch.z += Input.GetAxis("Mouse Y")*launchSens;
+			launch.x = Mathf.Clamp(launch.x, -1f, 1f);
+			launch.z = Mathf.Clamp(launch.z, 0, 2f);
+			Debug.DrawRay(stone.transform.position, launch);
+			//launch = launch.normalized*(launch.magnitude+Input.GetAxis("Mouse Y")*launchSensZ);
 		}
 	}
 
 	public void beginThrow()
 	{
 		//spawn stone
+		stone = GameObject.Instantiate(sPrefab, stoneSpawn);
+		camera.transform.parent = stone.transform;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		launch = new Vector3(0f,0f,1f);
+
+		mode = "launch";
 	}
 }
